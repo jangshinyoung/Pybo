20a4aad2ec705a6115bf0dbc33a603b52357d75a
장신영 <suupsigan@naver.com>
2021-05-14 14:54:22 +0900

    pybo.py 추가

---
+++ pybo.py
@@ -0,0 +1,5 @@
+from pybo import app
+
+
+if __name__ == '__name__':
+    app.run(host='0.0.0.0')
--- pybo/__init__.py
+++ pybo/__init__.py
@@ -9,26 +9,25 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app():  # 패키지 형태로 만듦.
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
-    # SQLAlchemy 설정
-    app.config.from_object(config)
-    db.init_app(app)
-    migrate.init_app(app,db)
+# SQLAlchemy 설정
+app.config.from_object(config)
+db.init_app(app)
+migrate.init_app(app,db)
 
-    from. import models
+from. import models
 
-    from .views import main_views,question_views,answer_views,auth_views,naverbook_views,movie_views
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(naverbook_views.bp)
-    app.register_blueprint(movie_views.bp)
+from .views import main_views,question_views,answer_views,auth_views,naverbook_views,movie_views
+app.register_blueprint(main_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(naverbook_views.bp)
+app.register_blueprint(movie_views.bp)
 
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+from .filter import format_datetime
+app.jinja_env.filters['datetime'] = format_datetime
 
-    return app
+
